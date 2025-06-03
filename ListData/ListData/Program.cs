7d0c1723e9bb4b86dbc7876790456a787a854a61
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-06-03 21:41:53 +0200

    Add names to the list using the Add method

---
+++ ListData/ListData/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string> names = ["<name>", "Ada", "Michal"];
+
+names.Add("John");
+names.Add("Jane");
+names.Add("Alice");
+names.Add("Bob");
+
+
+for (int i=0;i<names.Count; i++)
+{
+    Console.WriteLine($"hello {names[i].ToLower()}");
+}
+
+foreach( var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}");
+    
+}
