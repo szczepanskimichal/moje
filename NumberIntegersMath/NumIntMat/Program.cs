96e1edb94d2d18a6f7ea6e30664c48f41ca40bfb
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-06-03 15:16:05 +0200

    maths, int

--- NumberIntegersMath/NumIntMat/Program.cs
+++ NumberIntegersMath/NumIntMat/Program.cs
@@ -2,5 +2,9 @@
 int b = 5;
 int c = a / b;
 int d = a+ b;
+int e = 1200000000;
+int f = 1000000000;
 Console.WriteLine($"The result of {a} divided by {b} is {c}.");
 Console.WriteLine($"The result of {a} + {b} is {d}");
+
+long g = ((long)e + (long)f);
