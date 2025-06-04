e8ac41e23b64d74c235eae1c1a311f41ec934ff8
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-06-04 08:16:09 +0200

    Add element to string array using spread operator and demonstrate array iteration

--- ListData/ListData/Program.cs
+++ ListData/ListData/Program.cs
@@ -13,17 +13,19 @@ names.Add("Bob");
 
 
 
-Console.WriteLine(names[0]);
-Console.WriteLine(names[2]);
-Console.WriteLine(names.IndexOf("Ada"));
-Console.WriteLine(names.Contains("Michal"));
-Console.WriteLine(names.Count);
+//Console.WriteLine(names[0]);
+//Console.WriteLine(names[2]);
+//Console.WriteLine(names.IndexOf("Ada"));
+//Console.WriteLine(names.Contains("Michal"));
+//Console.WriteLine(names.Count);
 //Console.WriteLine(names[names.Count-1]);
-Console.WriteLine(names[^1]);//it's the same as names[names.Count-1]!!!Use index from end operator (^) to access the last element in the list
+//Console.WriteLine(names[^1]);//it's the same as names[names.Count-1]!!!Use index from end operator (^) to access the last element in the list/OSTATNI ELEMENT LISTY
+//Console.WriteLine(names[^2]);
 
-
-foreach (var name in names)
+var imiona = new string[] { "Ada", "Michal", "John", "Jane", "Alice", "Bob" }; // Array of strings!!!
+//imiona.Add("Kasia");//LIST<T>
+imiona = [..imiona, "Kasia"]; // Adding an element to an array creates a new array
+foreach (var name in imiona)
 {
-    Console.WriteLine($"Hello {name.ToUpper()}");
-
+    Console.WriteLine($"Hello {name.ToLower()}");
 }
