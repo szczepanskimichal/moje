61c39387186a71cc1ec864ed854663a71c858e1b
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-06-04 08:37:25 +0200

    Add sorting and modification of the names list • Changed imiona to a List<string> for easier element addition and sorting. • Used the Sort() method to alphabetically sort the names. • The code now prints a greeting for each name in the sorted list.

--- ListData/ListData/Program.cs
+++ ListData/ListData/Program.cs
@@ -1,9 +1,11 @@
-﻿List<string> names = ["<name>", "Ada", "Michal"];
+﻿using System.Collections.Immutable;
 
-names.Add("John");
-names.Add("Jane");
-names.Add("Alice");
-names.Add("Bob");
+//List<string> names = ["<name>", "Ada", "Michal"];
+
+//names.Add("John");
+//names.Add("Jane");
+//names.Add("Alice");
+//names.Add("Bob");
 
 
 //for (int i=0;i<names.Count; i++)
@@ -22,9 +24,12 @@ names.Add("Bob");
 //Console.WriteLine(names[^1]);//it's the same as names[names.Count-1]!!!Use index from end operator (^) to access the last element in the list/OSTATNI ELEMENT LISTY
 //Console.WriteLine(names[^2]);
 
-var imiona = new string[] { "Ada", "Michal", "John", "Jane", "Alice", "Bob" }; // Array of strings!!!
+var imiona = new List<string> { "Ada", "Michal", "John", "Jane", "Alice", "Bob" }; // Array of strings!!!
 //imiona.Add("Kasia");//LIST<T>
 imiona = [..imiona, "Kasia"]; // Adding an element to an array creates a new array
+
+imiona.Sort();
+
 foreach (var name in imiona)
 {
     Console.WriteLine($"Hello {name.ToLower()}");
