0b3ff2f39f7f7c0d89c20ee58315f6c3aa49923c
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-06-04 11:01:51 +0200

    Refactor LINQ query and update output formatting >> >> - Reformatted LINQ query for better readability. >> - Changed foreach loop to display each score with || delimiters. >> - Left commented-out List<int> code for potential future use.

---
+++ LINQ/linq/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//var numbers = new List<int> { 11, 12, 55, 77, 44,33};
+
+//Console.WriteLine( numbers.IndexOf(12));
+//numbers.Sort((a,b)=>b.CompareTo(a));
+//Console.WriteLine(string.Join("|| ", numbers));
+
+using System.Linq;
+
+int[] scores = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+//query expression syntax
+IEnumerable<int> Query =
+    from score in scores
+where score < 100
+select score;
+
+foreach (int i in Query)
+{
+    Console.WriteLine("||" + i + "||");
+}
