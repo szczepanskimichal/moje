a17e601136da353b01ae77e0eaab38225fbc02da
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-06-04 07:43:37 +0200

    Use index from end operator (^) to access the last element in the list

--- ListData/ListData/Program.cs
+++ ListData/ListData/Program.cs
@@ -6,15 +6,24 @@ names.Add("Alice");
 names.Add("Bob");
 
 
-for (int i=0;i<names.Count; i++)
-{
-    Console.WriteLine($"hello {names[i].ToLower()}");
-}
+//for (int i=0;i<names.Count; i++)
+//{
+//    Console.WriteLine($"hello {names[i].ToLower()}");
+//}
 
-foreach( var name in names)
+
+
+Console.WriteLine(names[0]);
+Console.WriteLine(names[2]);
+Console.WriteLine(names.IndexOf("Ada"));
+Console.WriteLine(names.Contains("Michal"));
+Console.WriteLine(names.Count);
+//Console.WriteLine(names[names.Count-1]);
+Console.WriteLine(names[^1]);//it's the same as names[names.Count-1]!!!Use index from end operator (^) to access the last element in the list
+
+
+foreach (var name in names)
 {
     Console.WriteLine($"Hello {name.ToUpper()}");
-    
-}
 
-//it seems to work, I just managed to resolve conflicts on GH, that's cool :)
+}
