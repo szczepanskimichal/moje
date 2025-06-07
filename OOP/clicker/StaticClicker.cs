185417183a1edc3a86105dfd29660a5a59d88440
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-06-07 13:13:30 +0200

    clicker uten OO

---
+++ OOP/clicker/StaticClicker.cs
@@ -0,0 +1,40 @@
+﻿public class StaticClicker
+{
+    public static int Points = 0;
+    public static int Upgrade = 1;
+    public static void Run()
+    {
+        //var points = 0;
+        //var upgrade = 1;
+
+        while (true)
+        {
+            ShowPoints();
+            var command = Console.ReadKey(false);
+            HandleCommand(command);
+            Console.WriteLine();
+        }
+    }
+
+    private static void HandleCommand(ConsoleKeyInfo command)
+    {
+        if (command.KeyChar == 'a')
+        {
+            Points += Upgrade;
+        }
+        else if (command.KeyChar == 'A')
+        {
+            if (Points > 0)
+            {
+                Upgrade++;
+                Points -= 10;
+            }
+        }
+    }
+
+    private static void ShowPoints()
+    {
+        Console.Clear();
+        Console.WriteLine($"Klikker A Du har {Points} poeng. (a=klikk, A=upgrade");
+    }
+}
